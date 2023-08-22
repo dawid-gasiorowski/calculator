6e2a1f253aaf8af4efa0a1b25351192cc4bde0a3
Dawid Gąsiorowski <dgasiorowski86@gmail.com>
2023-08-23 01:58:22 +0200

    Fix

--- src/components/ui/Panel.tsx
+++ src/components/ui/Panel.tsx
@@ -8,7 +8,7 @@ interface Props {
 const Panel = ({ header, children }: Props) => {
   return (
     <div className="panel">
-      <h1 className="panel-heading">{header}</h1>
+      <h1 className="panel-heading mb-5 has-text-centered">{header}</h1>
       <section className="container">{children}</section>
     </div>
   );
